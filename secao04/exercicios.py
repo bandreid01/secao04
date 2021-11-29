ad0ca5f9a731903249f1036c4fa27c95344f35d4
Vinicius da Silva <bandreid01@gmail.com>
2021-11-29 14:21:07 -0300

    Segundo exercício adicionado

--- secao04/exercicios.py
+++ secao04/exercicios.py
@@ -5,3 +5,11 @@ Ler um número inteiro e imprimir!
 n_int = int(4)
 print(n_int)
 print(type(n_int))
+
+"""
+Ler um número real e imprimir!
+"""
+
+n_real = float(3.4)
+print(n_real)
+print(type(n_real))
